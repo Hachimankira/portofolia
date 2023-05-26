58b3d076795a6c4e2a06b43771c401af56b8c0a8
Hachimnankira <kiran9860819025@gmail.com>
2023-05-26 23:34:45 +0545

    Edu and skills completed

--- src/data/Education/index.js
+++ src/data/Education/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import INFO from '../user';
 import EducationCard from './Components/EducationCard';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCogs, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+
 
 
 
@@ -12,8 +14,10 @@ const Education = () => {
         <div >
             <div className="d-flex gap-3 pb-5">
                 <div className="p-2 flex-fill border border-secondary-subtle rounded-lg border-opacity-50 border-3 rounded-3 p-3">
-                <i class="fa fa-graduation-cap"></i>
-                    <h3 className=' border-bottom'>Education</h3>
+                    <div className=' border-bottom gap-4' style={{display: 'flex', flexDirection: 'row'}}>
+                        <FontAwesomeIcon icon={faGraduationCap} />
+                        <h3>Education</h3>
+                    </div>
                     <div className='d-flex' style={{ display: 'flex', flexDirection: 'column' }}>
                         {
                             INFO.education.map((data => {
@@ -42,27 +46,31 @@ const Education = () => {
                 </div> */}
 
                 <div className="p-2 flex-fill border border-secondary-subtle rounded-lg border-opacity-50 border-3 rounded-3 p-3" style={{ textAlign: 'left' }}>
-                    <h3 className='border-bottom'>Software skills</h3>
+
+                <div className=' border-bottom gap-4' style={{display: 'flex', flexDirection: 'row'}}>
+                        <FontAwesomeIcon icon={faCogs} />
+                        <h3>Software Skills</h3>
+                    </div>
                     <div className='mt-2'>
-                    <div className='d-flex'>
-                        <div className='p-8 flex-fill'><h5>{INFO.software.skill1}</h5></div>
-                    </div>
-                    <br/>
-                    <div className='d-flex'>
-                        <div className='p-8 flex-fill'><h5>{INFO.software.skill2}</h5></div>
-                    </div>
-                    <br/>
-                    <div className='d-flex'>
-                        <div className='p-8 flex-fill'><h5>{INFO.software.skill3}</h5></div>
-                    </div>
-                    <br/>
-                    <div className='d-flex'>
-                        <div className='p-8 flex-fill'><h5>{INFO.software.skill4}</h5></div>
-                    </div>
-                    <br/>
-                    <div className='d-flex'>
-                        <div className='p-8 flex-fill'><h5>{INFO.software.skill5}</h5></div>
-                    </div>
+                        <div className='d-flex'>
+                            <div className='p-8 flex-fill'><h5>{INFO.software.skill1}</h5></div>
+                        </div>
+                        <br />
+                        <div className='d-flex'>
+                            <div className='p-8 flex-fill'><h5>{INFO.software.skill2}</h5></div>
+                        </div>
+                        <br />
+                        <div className='d-flex'>
+                            <div className='p-8 flex-fill'><h5>{INFO.software.skill3}</h5></div>
+                        </div>
+                        <br />
+                        <div className='d-flex'>
+                            <div className='p-8 flex-fill'><h5>{INFO.software.skill4}</h5></div>
+                        </div>
+                        <br />
+                        <div className='d-flex'>
+                            <div className='p-8 flex-fill'><h5>{INFO.software.skill5}</h5></div>
+                        </div>
                     </div>
                 </div>
 
